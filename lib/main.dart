3be358787776c6afdfc54258f3dba530c40a3ee9
Refaldy <refaldybanggana@gmail.com>
2024-04-27 10:43:33 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media/auth/login_or_register.dart';
+import 'package:social_media/firebase_options.dart';
 import 'package:social_media/theme/dark_mode.dart';
 import 'package:social_media/theme/light_mode.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
