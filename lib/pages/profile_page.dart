93d50de1fdc425151b382fd80c4b516ede42c8b7
Refaldy <refaldybanggana@gmail.com>
2024-04-28 20:23:19 +0700

    update profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media/components/my_back_button.dart';
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({super.key});
@@ -17,11 +18,6 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Profile"),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        elevation: 0,
-      ),
       backgroundColor: Theme.of(context).colorScheme.background,
       body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
         builder: (context, snapshot) {
@@ -43,7 +39,41 @@ class ProfilePage extends StatelessWidget {
 
             return Center(
               child: Column(
-                children: [Text(user!['email']), Text(user['username'])],
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 50, left: 25),
+                    child: Row(
+                      children: [
+                        MyBackButton(),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(24)),
+                    padding: const EdgeInsets.all(25),
+                    child: const Icon(
+                      Icons.person,
+                      size: 64,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    user!['username'],
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    user['email'],
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ],
               ),
             );
           } else {
--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media/helper/helper_function.dart';
 
 class UsersPage extends StatelessWidget {
   const UsersPage({super.key});
@@ -10,6 +12,39 @@ class UsersPage extends StatelessWidget {
         title: const Text("Users"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         elevation: 0,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: StreamBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            displayMessageToUser("Something went wrong", context);
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data == null) {
+            return const Text("No Data");
+          }
+
+          final users = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+
+              return ListTile(
+                title: Text(user['username']),
+                subtitle: Text(user['email']),
+              );
+            },
+          );
+        },
+        stream: FirebaseFirestore.instance.collection("Users").snapshots(),
       ),
     );
   }
